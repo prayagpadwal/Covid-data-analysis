31d6621b494a34337dcf666a35a745e08d181f3c
Prayag Padwal <65147413+prayagpadwal@users.noreply.github.com>
2023-02-08 10:46:30 -0800

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -99,5 +99,52 @@ Where dea.continent is not NULL
 -- order by 2, 3
 )
 
-SELECT *
+SELECT *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac
+
+
+-- Temp Table
+
+DROP table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+, SUM(CONVERT(int, vac.new_vaccinations)) OVER(Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+-- , (RollingPeopleVaccinated/population)*100
+
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+-- Where dea.continent is not NULL
+-- order by 2, 3
+
+SELECT *, (RollingPeopleVaccinated/Population)*100
+From #PercentPopulationVaccinated
+
+-- Creating view to store data for later visualization
+USE PortfolioProject
+GO
+Create view PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+, SUM(CONVERT(int, vac.new_vaccinations)) OVER(Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+-- , (RollingPeopleVaccinated/population)*100
+
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+Where dea.continent is not NULL
+-- order by 2, 3
+
+SELECT *
+From PercentPopulationVaccinated
